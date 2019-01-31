1ad2dbd3eba553130481ebc8b41a4ed4af80bdc0
Rust Jiang <rust0422@gmail.com>
2019-01-31 10:30:04 -0600

    Added Planet.java

---
+++ proj0/Planet.java
@@ -0,0 +1,27 @@
+public class Planet{
+	public double xxPos;
+	public double yyPos;
+	public double xxVel;
+	public double yyVel;
+	public double mass;
+	public String imgFileName;
+
+	public Planet(double xP, double yP, double xV,
+				  double yV, double m, String img){
+		xxPos = xP;
+		yyPos = yP;
+		xxVel = xV;
+		yyVel = yV;
+		mass  = m;
+		imgFileName = img;
+	}
+
+	public Planet(Planet P){
+		xxPos = P.xxPos;
+		yyPos = P.yyPos;
+		xxVel = P.xxVel;
+		yyVel = P.yyVel;
+		mass  = P.mass;
+		imgFileName = P.imgFileName;
+	}
+}
